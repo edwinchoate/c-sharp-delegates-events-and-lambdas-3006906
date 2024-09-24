f2dfd93d1600284003fea9fae1e90b9b63807dde
Edwin Choate <edwin.choate@pm.me>
2024-09-24 17:04:26 -0400

    2.1 Creating C# delegates

--- Start/Delegates/BasicDelegates/Program.cs
+++ Start/Delegates/BasicDelegates/Program.cs
@@ -3,7 +3,7 @@
 namespace BasicDelegates
 {
     // TODO: declare the delegate type
-
+    public delegate string MyDelegate(int arg1, int arg2);
 
     class MyClass
     {
@@ -18,15 +18,32 @@ namespace BasicDelegates
     class Program
     {
         // TODO: Create functions to serve as delegate implementations
+        public static string ConcatNumbers (int arg1, int arg2)
+        {
+            return arg1.ToString() + arg2.ToString();
+        }
 
+        public static string MultiplyNumbers (int arg1, int arg2)
+        {
+            return (arg1 * arg2).ToString();
+        }
 
         static void Main(string[] args)
         {
             // TODO: exercise each delegate function
+            MyDelegate f;
 
+            f = ConcatNumbers;
+            Console.WriteLine(f(3, 5));
+
+            f = MultiplyNumbers;
+            Console.WriteLine(f(3, 5));
 
             // TODO: Use an instance function of a class as a delegate
+            MyClass o = new MyClass();
 
+            f = o.instanceMethod1;
+            Console.WriteLine(f(3, 5));
 
         }
     }
